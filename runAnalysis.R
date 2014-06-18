46a08835fee85b887e715d328fa3a20baaf5c8fc
Muddsair <muddsairsharif@gmail.com>
2014-06-18 20:37:19 +0800

    Create runAnalysis.R and add some description

---
+++ runAnalysis.R
@@ -0,0 +1,9 @@
+##########################################################################################################
+
+## Coursera Getting and Cleaning Data Course Project
+
+##------------- R Programming --- Assignment_3-----------------------
+##------------- By Muddsair Sharif-----------------------------------
+##--------------muddsairsharif@gmail.com-----------------------------
+
+# runAnalysis.r File Description:
